b6c275a62bd9d784fea4913532dfc2ac6582d0f9
Bruna <bruna_gcarneiro@hotmail.com>
2023-07-11 15:30:22 -0300

    Adição de card no carrinho

---
+++ src/components/CartCards.tsx
@@ -0,0 +1,23 @@
+import '../styles/CartCards.css'
+
+interface CartCardsProps {
+    img: string;
+    productName: string;
+    productDescription: string; 
+    priceProduct: number;
+}
+
+export const CartCards = ({img, productName, productDescription, priceProduct}: CartCardsProps) => {
+    return (
+        <>
+        <div className="products-cart-container">
+            <img src={img} alt="Products" />
+            <div className="textCard">
+                <p className='productTitle'>{productName}</p>
+                <p className='productDescription'>{productDescription}</p>
+            </div>
+            <div className="priceProduct">{priceProduct}</div>
+        </div>
+        </>
+    )
+}
--- src/pages/cart/index.tsx
+++ src/pages/cart/index.tsx
@@ -1,12 +1,17 @@
 import '../../styles/Cart.css'
 import NavBar from '../../components/NavBar.tsx'
 import {PageTitle} from '../../components/PageTitle.tsx'
+import { CartCards } from '../../components/CartCards.tsx'
 
 export default function Cart(){ 
   return (
     <>
       <NavBar />
-      <PageTitle>Carrinho</PageTitle>
-    </>
+      <PageTitle>Meu Carrinho</PageTitle>
+      <div className="cart-cards-container">
+        <CartCards img="PizzaCogumelo.svg" productName="Pizza de Calabresa" productDescription="Calabresa, mussarela, tomate e orégano." priceProduct={29.90}/>
+        
+      </div>
+    </>   
   )
 }
