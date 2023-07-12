fc4ba29c9378f16984dea2a9c1c79c1973b63a15
Bruna <bruna_gcarneiro@hotmail.com>
2023-07-12 19:49:31 -0300

    adição do botão "registrar vendedor"

--- src/pages/sellers/index.tsx
+++ src/pages/sellers/index.tsx
@@ -2,7 +2,7 @@ import '../../styles/Sellers.css'
 import NavBar from '../../components/NavBar.tsx'
 import {PageTitle} from '../../components/PageTitle.tsx'
 import {SellerCards} from '../../components/SellerCards.tsx'
-
+import {RegisterButton} from '../../components/RegisterButton.tsx'
 
 export default function Sellers(){
     return (
@@ -10,6 +10,7 @@ export default function Sellers(){
           <NavBar />
           <PageTitle>Vendedores</PageTitle>
           <div className='sellers-container'>
+            <RegisterButton link = '/'>Cadastrar Vendedor</RegisterButton>
             <SellerCards sellerName='João Sousa da Silva' />
             <SellerCards sellerName='Maria do Socorro' />
             <SellerCards sellerName='Ana Francisca' />
