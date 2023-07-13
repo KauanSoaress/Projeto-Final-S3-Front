fe3c757ab64537c8d4f98612427d511f3ff7327f
Bruna <bruna_gcarneiro@hotmail.com>
2023-07-13 20:30:11 -0300

    adição listagem de vendedores

--- src/pages/sellers/index.tsx
+++ src/pages/sellers/index.tsx
@@ -3,17 +3,22 @@ import {NavBar} from '../../components/NavBar.tsx'
 import {PageTitle} from '../../components/PageTitle.tsx'
 import {SellerCards} from '../../components/SellerCards.tsx'
 import {RegisterButton} from '../../components/RegisterButton.tsx'
+import {UserArray} from '../../service/arrays/UserArray.ts'
+
 
 export default function Sellers(){
+  const userArray = UserArray.filter((user) => user.role === 'Vendedor');
     return (
         <>
           <NavBar />
           <PageTitle>Vendedores</PageTitle>
           <div className='sellers-container'>
             <RegisterButton link='/register-seller'>Cadastrar Vendedor</RegisterButton>
-            <SellerCards sellerName='João Sousa da Silva' />
-            <SellerCards sellerName='Maria do Socorro' />
-            <SellerCards sellerName='Ana Francisca' />
+            {
+              userArray.map((user) => {
+                return <SellerCards sellerName={user.name} />
+              })
+            }
           </div>
         </>
     )
